97f7deb0ce80d4c553db9acf136c3eeb69b055d6
bharath999_dev <bharath.bandlamudi@webvoidtechnologies.com>
2024-12-20 16:16:43 -0600

    initial commit

---
+++ src/main/java/com/dev/gorideapp/GoRideAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gorideapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRideAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoRideAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/gorideapp/GoRideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gorideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRideAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
